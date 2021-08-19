c4c3b24ca8a2d604432c1433975805afbae0dbcc
truongcutedz2309 <89027048+truongcutedz2309@users.noreply.github.com>
2021-08-19 18:43:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello Bro !");
+console.log("Hello 🌎");
 
 /* 
 Make the "Click me!" button move when the visitor clicks it:
